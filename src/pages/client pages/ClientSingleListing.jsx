2292547f8275844c14d7dc4776628e01f57d472a
Kimball Goss <Kimballgoss@gmail.com>
2024-08-15 15:19:55 -0600

    listing creation page styled

--- src/pages/client pages/ClientSingleListing.jsx
+++ src/pages/client pages/ClientSingleListing.jsx
@@ -40,27 +40,45 @@ function ClientSingleListing() {
   if (editing === false) {
     //Render this if they are not in edit mode
     return (
-      <>
-        <h1>Listing #{listing.listingId}</h1>
-        <p>
-          This page should show a single listing with all of it's details. There
-          should be a delete button at the bottom, which would delete the
-          listing. There should also be an edit button which turns every feild
-          into an editable field and adds a "save changes" button and "cancel"
-          button at the bottom. Links to the pilots profile should be provided,
-          as well as a button that allows the client to create a new new listing
-          based on this completed one.
-        </p>
-        <div>
-          <h3>Listing Id: {listing.listingId}</h3>
-          <h3>Owner Id: {listing.clientId}</h3>
-          <br />
-          <p>Date: {listing.date}</p>
-          <p>Flight Address: {listing.flightAddress}</p>
-          <p>Description: {listing.description}</p>
-          <table>
+      <div className="flex flex-col items-center">
+
+        <div className="flex flex-col items-center pt-10 pb-10">
+          <h1 className=" font-rubik font-medium text-[50px] text-AJGO_DarkSlateGray justify-center">Listing #{listing.listingId}</h1>
+          <p className="font-rubik text-xl">
+            This show's all the details for listing {listing.listingId}, here you can edit or delete the listing, as well as accept or deny applications
+          </p>
+        </div>
+
+        <div className="flex pt-10 pb-10 justify-between font-rubik font-medium text-[25px] text-AJGO_DarkSlateGray">
+
+          <div className="pr-10 pl-10">
+            <h3>Listing Id: {listing.listingId}</h3>
+            <h3>Owner Id: <button
+              onClick={() => {
+                navigate(`/userProfile/client/${listing.clientId}`)
+              }}
+              className="border-2 border-ADJO_Keppel opacity-70 rounded-full h-8 w-20 text-ADJO_Keppel text-lg font-medium"
+            >
+              {" "}
+              {listing.clientId}
+            </button></h3>
+          </div>
+
+          <div>
+            <p>Flight Date: {listing.flightDate}</p>
+            <p>Flight Address: {listing.flightAddress}</p>
+          </div>
+
+        </div>
+
+        <div className="flex pt-10 pb-10 font-rubik font-medium w-3/5 justify-center">
+          <p className="text-center">Description: {listing.description}</p>
+        </div>
+
+        <div className="flex justify-center bg-ADJO_Celeste bg-opacity-30 rounded-xl w-11/12 pr-10 pl-10 pt-5 pb-5">
+          <table className="table-auto border-collapse font-rubik pb-20">
             <thead>
-              <tr>
+              <tr className="border-b-4 border-opacity-30 border-b-AJGO_DarkSlateGray">
                 <th>Offering</th>
                 <th>Multi-Day Operation</th>
                 <th>Providing Hardware</th>
@@ -85,9 +103,12 @@ function ClientSingleListing() {
         </div>
 
         {listing.clientId === userId && (
-          <div>
-            <button onClick={handleEditButton}>Edit Listing</button>
+          <div className=" w-1/2 pt-10 pb-10 flex justify-around">
             <button
+              className="bg-ADJO_Keppel px-8 py-1 text-xl text- uppercase font-rubik rounded-lg"
+              onClick={handleEditButton}>Edit Listing</button>
+            <button
+              className="bg-ADJO_Keppel px-8 py-1 text-xl text- uppercase font-rubik rounded-lg"
               onClick={() => {
                 //This will delete the listing we are currently viewing from the database, and then redirect the user back to their listings
                 axios.delete(`/api/listing/${listing.listingId}`).then(() => {
@@ -99,7 +120,7 @@ function ClientSingleListing() {
             </button>
           </div>
         )}
-      </>
+      </div>
     )
   } else if (editing === true) {
     //Render this if they clicked the edit button
@@ -128,130 +149,186 @@ function ClientSingleListing() {
         })
     }
     return (
-      <>
-        <h1>Edit this Listing</h1>
-        <p>
-          This page is one giant form that allows a client to edit an already
-          existing listing. The buttons at the bottom should be "save changes",
-          and "cancel"
-        </p>
-        <form onSubmit={handleListingEdit}>
-          <label htmlFor="clientId">Client ID: {userId}</label>
+      <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center pt-10 pb-10">
+          <h1 className=" font-rubik font-medium text-[50px] text-AJGO_DarkSlateGray justify-center">Edit this Listing</h1>
+          <p className="font-rubik text-xl">
+            Here you can change any property on this listing, including marking it as complete
+          </p>
+        </div>
+
+        <form className="font-rubik text-xl bg-ADJO_Celeste p-5 rounded-lg w-3/5">
+
+          <div >
+            <div className="flex justify-center font-rubik font-medium text-[25px] text-AJGO_DarkSlateGray ">
+              <label htmlFor="listingId">Listing #{listing.listingId}</label>
+            </div>
+            <div className="flex justify-center">
+              <label htmlFor="clientId">Client {userId}</label>
+            </div>
+          </div>
+
           <br />
-          <label htmlFor="offer">Offer: </label>
-          <input
-            type="number"
-            name="offer"
-            value={offer}
-            onChange={(e) => setOffer(e.target.value)}
-            required
-          ></input>
+
+          <div className="pb-1 pt-1">
+            <label htmlFor="offer">Offer: $</label>
+            <input
+              className="pl-2 pt-1 pb-1 w-24 rounded-lg ring-2 ring-inset ring-[#9ca3af] focus-within:ring-4 focus-within:ring-inset focus-within:ring-ADJO_Keppel"
+              type="number"
+              name="offer"
+              value={offer}
+              step={5}
+              onChange={(e) => setOffer(Number(e.target.value).toFixed(2))}
+              required
+            ></input>
+          </div>
+
+          <div className="pb-1 pt-1">
+            <label htmlFor="flightDate" required>
+              Flight Date:{" "}
+            </label>
+            <input
+              className="pl-2 pt-1 pb-1 w-44 rounded-lg ring-2 ring-inset ring-[#9ca3af] focus-within:ring-4 focus-within:ring-inset focus-within:ring-ADJO_Keppel"
+              type="date"
+              name="flightDate"
+              value={flightDate}
+              onChange={(e) => setFlightDate(e.target.value)}
+            ></input>
+          </div>
+
+          <div className="pb-1 pt-1">
+            <label htmlFor="flightAddress">Flight Address: </label>
+            <input
+              className="pl-2 pt-1 pb-1 w-4/5 rounded-lg ring-2 ring-inset ring-[#9ca3af] focus-within:ring-4 focus-within:ring-inset focus-within:ring-ADJO_Keppel"
+              type="text" pb-1 pt-1
+              name="flightAddress"
+              value={flightAddress}
+              onChange={(e) => setFlightAddress(e.target.value)}
+              required
+            ></input>
+          </div>
+
+          <div className="flex pb-1 pt-1 items-center">
+            <label htmlFor="flightRadius">Flight Radius: </label>
+            <input
+              className="pl-2 pt-1 pb-1 w-16 rounded-lg ring-2 ring-inset ring-[#9ca3af] focus-within:ring-4 focus-within:ring-inset focus-within:ring-ADJO_Keppel"
+              type="number"
+              name="flightRadius"
+              step={0.1}
+              min={0.1}
+              max={5}
+              value={flightRadius}
+              onChange={(e) => setFlightRadius(Number(e.target.value).toFixed(1))}
+              required
+            ></input>
+            <label htmlFor="flightRadius"> miles </label>
+          </div>
+
           <br />
-          <label htmlFor="flightDate" required>
-            Flight Date:{" "}
-          </label>
-          <input
-            type="date"
-            name="flightDate"
-            value={flightDate}
-            onChange={(e) => setFlightDate(e.target.value)}
-          ></input>
+
+          <div className="flex flex-col items-center">
+            <div>
+              <input
+                type="checkbox"
+                className="h-5 w-5 checked:bg-ADJO_Keppel text-ADJO_Keppel hover:ring-2 hover:ring-inset hover:ring-ADJO_Keppel"
+                name="multiday"
+                checked={multiday}
+                onChange={(e) => setMultiday(e.target.checked)}
+              ></input>
+              <label className="pl-3" htmlFor="multiday">
+                This operation will take multiple days to complete
+              </label>
+            </div>
+            <br />
+            <div>
+              <input
+                type="checkbox"
+                className="h-5 w-5 checked:bg-ADJO_Keppel text-ADJO_Keppel hover:ring-2 hover:ring-inset hover:ring-ADJO_Keppel"
+                name="hardwareProvided"
+                checked={hardwareProvided}
+                onChange={(e) => setHardware(e.target.checked)}
+              ></input>
+              <label className="pl-3" htmlFor="hardwareProvided">
+                I can provide some or all of the hardware needed
+              </label>
+            </div>
+            <br />
+            <div>
+              <input
+                type="checkbox"
+                className="h-5 w-5 checked:bg-ADJO_Keppel text-ADJO_Keppel hover:ring-2 hover:ring-inset hover:ring-ADJO_Keppel"
+                name="softwareProvided"
+                checked={softwareProvided}
+                onChange={(e) => setSoftware(e.target.checked)}
+              ></input>
+              <label className="pl-3" htmlFor="softwareProvided">
+                I can provide some or all of the software needed
+              </label>
+            </div>
+            <br />
+            <div>
+              <input
+                type="checkbox"
+                className="h-5 w-5 checked:bg-ADJO_Keppel text-ADJO_Keppel hover:ring-2 hover:ring-inset hover:ring-ADJO_Keppel"
+                name="nightFlying"
+                checked={nightFlying}
+                onChange={(e) => setNightflying(e.target.checked)}
+              ></input>
+              <label className="pl-3" htmlFor="nightFlying">
+                This job requires flying at dusk, dawn, or at night
+              </label>
+            </div>
+            <br />
+            <div>
+              <input
+                type="checkbox"
+                className="h-5 w-5 checked:bg-ADJO_Keppel text-ADJO_Keppel hover:ring-2 hover:ring-inset hover:ring-ADJO_Keppel"
+                name="crowdFlying"
+                checked={crowdFlying}
+                onChange={(e) => setCrowdflying(e.target.checked)}
+              ></input>
+              <label className="pl-3" htmlFor="crowdFlying">
+                This job requires flying over people
+              </label>
+            </div>
+          </div>
+
           <br />
-          <input
-            type="checkbox"
-            name="multiday"
-            checked={multiday}
-            onChange={(e) => setMultiday(e.target.checked)}
-          ></input>
-          <label htmlFor="multiday">
-            This operation will take multiple days to complete
-          </label>
-          <br />
-          <label htmlFor="flightAddress">Flight Address: </label>
-          <input
-            type="text"
-            name="flightAddress"
-            value={flightAddress}
-            onChange={(e) => setFlightAddress(e.target.value)}
-            required
-          ></input>
-          <br />
-          <label htmlFor="flightRadius">Flight Radius in miles:</label>
-          <input
-            type="number"
-            name="flightRadius"
-            step={0.1}
-            min={0.1}
-            value={flightRadius}
-            onChange={(e) => setFlightRadius(Number(e.target.value).toFixed(1))}
-            required
-          ></input>
-          <br />
-          <input
-            type="checkbox"
-            name="hardwareProvided"
-            checked={hardwareProvided}
-            onChange={(e) => setHardware(e.target.checked)}
-          ></input>
-          <label htmlFor="hardwareProvided">
-            I can provide some or all of the hardware needed
-          </label>
-          <br />
-          <input
-            type="checkbox"
-            name="softwareProvided"
-            checked={softwareProvided}
-            onChange={(e) => setSoftware(e.target.checked)}
-          ></input>
-          <label htmlFor="softwareProvided">
-            I can provide some or all of the software needed
-          </label>
-          <br />
-          <input
-            type="checkbox"
-            name="nightFlying"
-            checked={nightFlying}
-            onChange={(e) => setNightflying(e.target.checked)}
-          ></input>
-          <label htmlFor="nightFlying">
-            This job requires flying at dusk, dawn, or at night
-          </label>
-          <br />
-          <input
-            type="checkbox"
-            name="crowdFlying"
-            checked={crowdFlying}
-            onChange={(e) => setCrowdflying(e.target.checked)}
-          ></input>
-          <label htmlFor="crowdFlying">
-            This job requires flying over people who are unaware of the
-            operation
-          </label>
-          <br />
-          <br />
-          <label htmlFor="description">Details about this job: </label>
-          <br />
-          <input
-            type="textarea"
-            name="description"
-            rows="10"
-            cols="50"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          ></input>
-          <br />
-          <input
-            type="checkbox"
-            name="completed"
-            check={completed}
-            onChange={(e) => setCompleted(e.target.checked)}
-          ></input>
-          <label htmlFor="completed">Job is completed</label>
-          <br />
-          <input type="submit" value="Save Changes"></input>
+
+          <div className="flex flex-col items-center">
+            <label htmlFor="description">Details about this job: </label>
+            <br />
+            <textarea
+              className="p-3 rounded-lg ring-2 ring-inset ring-[#9ca3af] focus-within:ring-4 focus-within:ring-inset focus-within:ring-ADJO_Keppel"
+              name="description"
+              rows="10"
+              cols="50"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            ></textarea>
+            <br />
+            <div>
+              <input
+                type="checkbox"
+                className="h-5 w-5 checked:bg-ADJO_Keppel text-ADJO_Keppel hover:ring-2 hover:ring-inset hover:ring-ADJO_Keppel"
+                name="completed"
+                check={completed}
+                onChange={(e) => setCompleted(e.target.checked)}
+              ></input>
+              <label className="pl-3" htmlFor="completed">This Job is complete</label>
+            </div>
+          </div>
         </form>
-        <button onClick={handleEditButton}>Discard Changes</button>
-      </>
+        <div className="pt-10 pb-10 flex w-3/5 justify-around">
+          <button
+            className="bg-ADJO_Keppel px-8 py-1 text-xl text- uppercase font-rubik rounded-lg"
+            onClick={handleListingEdit}>Save Changes</button>
+          <button
+            className="bg-ADJO_Keppel px-8 py-1 text-xl text- uppercase font-rubik rounded-lg"
+            onClick={handleEditButton}>Cancel</button>
+
+        </div>
+      </div>
     )
   }
 }
--- src/pages/client pages/NewListing.jsx
+++ src/pages/client pages/NewListing.jsx
@@ -9,7 +9,7 @@ function NewListing() {
   const navigate = useNavigate()
 
   const [offer, setOffer] = useState(10.00)
-  const [flightDate, setFlightDate] = useState(2025-12-25)
+  const [flightDate, setFlightDate] = useState("2009-09-02")
   const [multiday, setMultiday] = useState(false)
   const [flightAddress, setFlightAddress] = useState("123 Blue Sky Way")
   const [flightRadius, setFlightRadius] = useState(0.1)
@@ -47,134 +47,169 @@ function NewListing() {
 
   if (usertype === "client") {
     return (
-      <>
-        <h1>Create a New Listing</h1>
-        <p>
-          This page is one giant form that allows a client to create an entirely
-          new listing, if we were redirected here from a "completed" listing, it
-          should inherit all of those values, only requiring a new flight date.
-          The buttons at the bottom should be "submit", "save draft", and
-          "cancel".
-        </p>
-        <form onSubmit={handleListingSubmission}>
-          <label htmlFor="clientId">Client ID: {userid}</label>
+      <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center pt-10 pb-10">
+          <h1 className=" font-rubik font-medium text-[50px] text-AJGO_DarkSlateGray justify-center">Create a New Listing</h1>
+          <p className="font-rubik text-xl">
+            Fill out the fields below to submit your listing
+          </p>
+        </div>
+
+        <form className="font-rubik text-xl bg-ADJO_Celeste p-5 rounded-lg w-3/5">
+
+          <div className="flex justify-center">
+            <label htmlFor="clientId">Client ID: {userid}</label>
+          </div>
+
           <br />
-          <label htmlFor="offer" required>
-            Offer:{" "}
-          </label>
-          <input
-            type="number"
-            name="offer"
-            value={offer}
-            onChange={(e) => setOffer(e.target.value)}
-            required
-          ></input>
+
+          <div className="pb-1 pt-1">
+            <label htmlFor="offer" required>
+              Offer: ${" "}
+            </label>
+            <input
+              className="pl-2 pt-1 pb-1 w-24 rounded-lg ring-2 ring-inset ring-[#9ca3af] focus-within:ring-4 focus-within:ring-inset focus-within:ring-ADJO_Keppel"
+              type="number"
+              name="offer"
+              value={offer}
+              step={5}
+              onChange={(e) => setOffer(Number(e.target.value).toFixed(2))}
+              required
+            ></input>
+          </div>
+
+          <div className="pb-1 pt-1">
+            <label htmlFor="flightDate" required>
+              Flight Date:{" "}
+            </label>
+            <input
+              className="pl-2 pt-1 pb-1 w-44 rounded-lg ring-2 ring-inset ring-[#9ca3af] focus-within:ring-4 focus-within:ring-inset focus-within:ring-ADJO_Keppel"
+              type="date"
+              name="flightDate"
+              value={flightDate}
+              onChange={(e) => setFlightDate(e.target.value)}
+            ></input>
+          </div>
+
+          <div className="pb-1 pt-1">
+            <label htmlFor="flightAddress">Flight Address: </label>
+            <input
+              className="pl-2 pt-1 pb-1 w-4/5 rounded-lg ring-2 ring-inset ring-[#9ca3af] focus-within:ring-4 focus-within:ring-inset focus-within:ring-ADJO_Keppel"
+              type="text"
+              name="flightAddress"
+              value={flightAddress}
+              onChange={(e) => setFlightAddress(e.target.value)}
+              required
+            ></input>
+          </div>
+
+          <div>
+            <label htmlFor="flightRadius">Flight Radius in miles:</label>
+            <input
+              className="pl-2 pt-1 pb-1 w-16 rounded-lg ring-2 ring-inset ring-[#9ca3af] focus-within:ring-4 focus-within:ring-inset focus-within:ring-ADJO_Keppel"
+              type="number"
+              name="flightRadius"
+              step={0.1}
+              min={0.1}
+              max={5}
+              value={flightRadius}
+              onChange={(e) => setFlightRadius(Number(e.target.value).toFixed(1))}
+              required
+            ></input>
+          </div>
+
           <br />
-          <label htmlFor="flightDate" required>
-            Flight Date:{" "}
-          </label>
-          <input
-            type="date"
-            name="flightDate"
-            value={flightDate}
-            onChange={(e) => setFlightDate(e.target.value)}
-          ></input>
+
+          <div className="flex flex-col items-center">
+            <div>
+              <input
+                type="checkbox"
+                className="h-5 w-5 checked:bg-ADJO_Keppel text-ADJO_Keppel hover:ring-2 hover:ring-inset hover:ring-ADJO_Keppel"
+                name="multiday"
+                checked={multiday}
+                onChange={(e) => setMultiday(e.target.value)}
+              ></input>
+              <label className="pl-3" htmlFor="multiday">
+                This operation will take multiple days to complete
+              </label>
+            </div>
+            <br />
+            <div>
+              <input
+                type="checkbox"
+                className="h-5 w-5 checked:bg-ADJO_Keppel text-ADJO_Keppel hover:ring-2 hover:ring-inset hover:ring-ADJO_Keppel"
+                name="hardwareProvided"
+                checked={hardwareProvided}
+                onChange={(e) => setHardware(e.target.value)}
+              ></input>
+              <label className="pl-3" htmlFor="hardwareProvided">
+                I can provide some or all of the hardware needed
+              </label>
+            </div>
+            <br />
+            <div>
+              <input
+                type="checkbox"
+                className="h-5 w-5 checked:bg-ADJO_Keppel text-ADJO_Keppel hover:ring-2 hover:ring-inset hover:ring-ADJO_Keppel"
+                name="softwareProvided"
+                checked={softwareProvided}
+                onChange={(e) => setSoftware(e.target.value)}
+              ></input>
+              <label className="pl-3" htmlFor="softwareProvided">
+                I can provide some or all of the software needed
+              </label>
+            </div>
+            <br />
+            <div>
+              <input
+                className="h-5 w-5 checked:bg-ADJO_Keppel text-ADJO_Keppel hover:ring-2 hover:ring-inset hover:ring-ADJO_Keppel"
+                type="checkbox"
+                name="nightFlying"
+                checked={nightFlying}
+                onChange={(e) => setNightflying(e.target.value)}
+              ></input>
+              <label className="pl-3" htmlFor="nightFlying">
+                This job requires flying at dusk, dawn, or at night
+              </label>
+            </div>
+            <br />
+            <div>
+              <input
+                className="h-5 w-5 checked:bg-ADJO_Keppel text-ADJO_Keppel hover:ring-2 hover:ring-inset hover:ring-ADJO_Keppel"
+                type="checkbox"
+                name="crowdFlying"
+                checked={crowdFlying}
+                onChange={(e) => setCrowdflying(e.target.value)}
+              ></input>
+              <label className="pl-3" htmlFor="crowdFlying">
+                This job requires flying over people
+              </label>
+            </div>
+          </div>
+
           <br />
-          <input
-            type="checkbox"
-            name="multiday"
-            checked={multiday}
-            onChange={(e) => setMultiday(e.target.value)}
-          ></input>
-          <label htmlFor="multiday">
-            This operation will take multiple days to complete
-          </label>
-          <br />
-          <label htmlFor="flightAddress">Flight Address: </label>
-          <input
-            type="text"
-            name="flightAddress"
-            value={flightAddress}
-            onChange={(e) => setFlightAddress(e.target.value)}
-            required
-          ></input>
-          <br />
-          <label htmlFor="flightRadius">Flight Radius in miles:</label>
-          <input
-            type="number"
-            name="flightRadius"
-            step= {0.1}
-            min ={0.1}
-            value={flightRadius}
-            onChange={(e) => setFlightRadius(Number(e.target.value).toFixed(1))}
-            required
-          ></input>
-          <br />
-          <input
-            type="checkbox"
-            name="hardwareProvided"
-            checked={hardwareProvided}
-            onChange={(e) => setHardware(e.target.value)}
-          ></input>
-          <label htmlFor="hardwareProvided">
-            I can provide some or all of the hardware needed
-          </label>
-          <br />
-          <input
-            type="checkbox"
-            name="softwareProvided"
-            checked={softwareProvided}
-            onChange={(e) => setSoftware(e.target.value)}
-          ></input>
-          <label htmlFor="softwareProvided">
-            I can provide some or all of the software needed
-          </label>
-          <br />
-          <input
-            type="checkbox"
-            name="nightFlying"
-            checked={nightFlying}
-            onChange={(e) => setNightflying(e.target.value)}
-          ></input>
-          <label htmlFor="nightFlying">
-            This job requires flying at dusk, dawn, or at night
-          </label>
-          <br />
-          <input
-            type="checkbox"
-            name="crowdFlying"
-            checked={crowdFlying}
-            onChange={(e) => setCrowdflying(e.target.value)}
-          ></input>
-          <label htmlFor="crowdFlying">
-            This job requires flying over people who are unaware of the
-            operation
-          </label>
-          <br />
-          <br />
-          <label htmlFor="description">Details about this job: </label>
-          <br />
-          <input
-            type="textarea"
-            name="description"
-            rows="10"
-            cols="50"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          ></input>
-          <br />
-          <br />
-          <br />
-          <input
-            type="button"
-            name="saveDraft"
-            id="saveDraft"
-            value="Save Draft"
-          />
-          <br />
-          <input type="submit" value="Submit New Listing"></input>
+
+          <div className="flex flex-col items-center">
+            <label htmlFor="description">Details about this job: </label>
+            <br />
+            <textarea
+              className="p-3 rounded-lg ring-2 ring-inset ring-[#9ca3af] focus-within:ring-4 focus-within:ring-inset focus-within:ring-ADJO_Keppel" name="description"
+              rows="10"
+              cols="50"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            ></textarea>
+          </div>
         </form>
-      </>
+        <div className="pt-10 pb-10 flex w-3/5 justify-around">
+          <button
+            className="bg-ADJO_Keppel px-8 py-1 text-xl text- uppercase font-rubik rounded-lg"
+            onClick={()=>navigate("/myListings")}>Cancel</button>
+          <button
+            className="bg-ADJO_Keppel px-8 py-1 text-xl text- uppercase font-rubik rounded-lg"
+            onClick={handleListingSubmission}>Submit</button>
+        </div>
+      </div>
     )
   } else {
     return (
